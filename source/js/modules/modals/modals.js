db99fb1dcaa92631b1a503ef2bb290b8966408cd
mxz22345 <105110779+mxz22345@users.noreply.github.com>
2023-03-29 21:33:54 +0600

    доработка js

--- source/js/modules/modals/modals.js
+++ source/js/modules/modals/modals.js
@@ -2,17 +2,21 @@ const modal = document.querySelector('.modal-call');
 const modalOpenElement = document.querySelector('.nav__button');
 const modalCloseElement = modal.querySelector('.modal-call__button-close');
 
-modalOpenElement.addEventListener('click', () => {
-  modal.classList.remove('visually-hidden');
-});
+if (modal) {
+  modalOpenElement.addEventListener('click', () => {
+    modal.classList.remove('visually-hidden');
+  });
 
-modalCloseElement.addEventListener('click', () => {
-  modal.classList.add('visually-hidden');
-});
-
-document.addEventListener('keydown', (evt) => {
-  if (evt.key === 'Escape') {
-    evt.preventDefault();
+  modalCloseElement.addEventListener('click', () => {
     modal.classList.add('visually-hidden');
-  }
-});
+  });
+
+  document.addEventListener('keydown', (evt) => {
+    if (evt.key === 'Escape') {
+      evt.preventDefault();
+      modal.classList.add('visually-hidden');
+    }
+  });
+}
+
+
