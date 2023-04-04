408cfa70d963e16e7cbab3b28d35d17518806645
mxz22345 <105110779+mxz22345@users.noreply.github.com>
2023-04-04 12:41:53 +0600

    доработка modal

--- source/js/modules/modals/modals.js
+++ source/js/modules/modals/modals.js
@@ -5,10 +5,12 @@ const modalCloseElement = modal.querySelector('.modal-call__button-close');
 if (modal) {
   modalOpenElement.addEventListener('click', () => {
     modal.classList.remove('visually-hidden');
+    document.body.style.overflow = 'hidden';
   });
 
   modalCloseElement.addEventListener('click', () => {
     modal.classList.add('visually-hidden');
+    document.body.style.overflow = '';
   });
 
   document.addEventListener('keydown', (evt) => {
